9ce4fda9a1af8a1e3a5001958034da5fad9d6b7e
Zahid Hossain <60911932+zhossain-info@users.noreply.github.com>
2022-07-31 18:29:10 +0600

    update schema and json file example

--- src/main/resources/schema.scm
+++ src/main/resources/schema.scm
@@ -1,9 +1,14 @@
 import org.json.assertion.lib.CoreFunction
 
+/*
+There will be some details about this schema file
+including version and modification date etc.
+*/
+
 {
     "testInteger" : #integer,
     "testString" : #string,
-    "testFloat" : #float,           //allow comments
+    "testFloat" : #float,           //this is a line comments
     "testDecimal" : #decimal,
     "testAnyTypeOfNumber" : #number,
     "testObject" : {
@@ -12,8 +17,8 @@ import org.json.assertion.lib.CoreFunction
             @containsKeys("testInnerBoolean", "testExactValueMatch")
     },
     "testFunction" : @minMax(1, 10)#integer,
-    "testOptionalDataType" : ?#integer,
-    "testOptionalFunction" : ?@minMax(1, 10),
-    "testOptionalAvailable" : ?#string,
+    "testOptionalWithDataType" : ?#integer,
+    "testOptionalWithFunction" : ?@minMax(1, 10),
+    "testOptionalWhenInputAvailable" : ?#string,
     "testArray" : [1, @minMax(1, 50), 3, @containsAt(0, 3, 2, 1)]
 }
